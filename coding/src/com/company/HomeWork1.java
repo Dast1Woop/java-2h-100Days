9ca9d39a5fcea5068efc42ad6b10328e8c4bc8c3
myl <myldast1@126.com>
2021-10-12 17:51:47 +0800

    chap3 homeworks done

---
+++ coding/src/com/company/HomeWork1.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.awt.*;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        System.out.println("wow");
+
+        int n1 = 13, n2 = 17;
+        int n3 = n1 + n2;
+        int n4 = 48;
+        int n5 = n4 - n3;
+        System.out.println(n3);
+        System.out.println(n5);
+    }
+}
---
+++ stringInt.java
@@ -0,0 +1,9 @@
+class stringInt{
+	public static void main(String[] args) {
+	int a = 1;
+	String aS = a + "";
+	System.out.println(aS);
+
+}	
+}
+  	
